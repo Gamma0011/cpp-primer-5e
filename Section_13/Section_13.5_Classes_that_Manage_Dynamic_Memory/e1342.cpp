e13bbfd0e26f07ef60f41efd9cd4f32e8267e1c7
Gamma0011 <44280901+Gamma0011@users.noreply.github.com>
2021-01-28 09:36:36 -0500

    01/28/21 Practice

--- Section_13/Section_13.5_Classes_that_Manage_Dynamic_Memory/e1342.cpp
+++ Section_13/Section_13.5_Classes_that_Manage_Dynamic_Memory/e1342.cpp
@@ -17,20 +17,23 @@ class StrVec {
 public:
     std::size_t size() { return cap - beg; }                        // total memory allocated
     std::size_t capacity() { return cap - lelem; }                  // space remaining
-    std::size_t reserve() { return (size() ? size() * 2 : 1); }     // reserve 2x current size for reallocation
+    std::size_t reserve(std::size_t sz) { return (sz ? size() * 2 : 1); }     // reserve 2x current size for reallocation
     auto resize(std::size_t sz) { return alloc.allocate(sz); }      // return pointer to newly created block of raw data
     std::string *begin() const { return beg; }
     std::string *end() const { return lelem; }
 
     StrVec() : beg(nullptr), lelem(nullptr), cap(nullptr) { };  // default constructor
     StrVec(std::initializer_list<std::string> il) {
+        reallocate(il.size());
+        /*
         auto ns = resize(il.size());
         auto el(ns);                    // el will be used to increment
+        */
 
-        for (auto s : il) { alloc.construct(el++, s); }     // at position el, insert s
+        for (auto s : il) { alloc.construct(beg++, s); }     // at position el, insert s
         // at end of iteration through initializer_list, el == end
-        beg = ns;           // ns denotes start of new strings
-        lelem = cap = el;   // el denotes last element and cap of new strings
+        //beg = ns;           // ns denotes start of new strings
+        //lelem = cap = el;   // el denotes last element and cap of new strings
     }
 
     StrVec(const StrVec &);             // copy constructor
@@ -46,7 +49,7 @@ private:
     std::string *cap;       // points to one past last element of array
 
     void check_n_allocate();
-    void reallocate();      // reallocate current strings to new block of memory
+    void reallocate(std::size_t);      // reallocate current strings to new block of memory
     void free();            // free old memory
     std::pair<std::string*, std::string*> alloc_n_copy
         (const std::string*, const std::string*);
@@ -84,25 +87,24 @@ void StrVec::push_back(const std::string &s) {
 /***************** StrVec PRIVATE MEMBER FUNCTIONS *****************/
 void StrVec::check_n_allocate() {
     if (size() == capacity()) { 
-        reallocate();
+        reallocate(size());
     }
 }
 
-void StrVec::reallocate() {
-    auto sz = reserve();
-    auto newblock = resize(sz);
-
-    auto dest = newblock;
-    auto source = beg;
+void StrVec::reallocate(std::size_t sz) {
+    auto newblock = (sz) ? sz : (size()) ? 2 * size() : 1;
+    auto newdata = alloc.allocate(newblock);
+    auto dest = newdata;
+    auto elem = beg;
 
     for (auto i = 0; i != size() ; ++i) {
-        alloc.construct(++dest, std::move(*source++));
+        alloc.construct(++dest, std::move(*elem++));
     }
     free();
 
-    beg = newblock;         // first element of string array
+    beg = newdata;         // first element of string array
     lelem = dest;           // final element when for loop ended
-    cap = beg + sz;         // beg element + size of elements allocated
+    cap = beg + newblock;         // beg element + size of elements allocated
 
 }
 
@@ -111,8 +113,8 @@ void StrVec::free() {
         for ( auto e = lelem; e != beg; ) {
             alloc.destroy(--e);
         }
-    }
     alloc.deallocate(beg, cap - beg);
+    }
 }
 
 std::pair<std::string*, std::string*> 
@@ -126,8 +128,9 @@ std::string make_plural(std::size_t, const std::string &, const std::string &);
 
 class TextQuery {
 public:
-    TextQuery(std::ifstream &ifs) : svec(new std::vector<std::string>) {
+    TextQuery(std::ifstream &ifs) : svec(new StrVec) {
         for (std::string line ; std::getline(ifs, line) ; svec->push_back(line)) {
+            /*
             std::string word;
             std::vector<std::string>::size_type n = svec->size();
             for(std::istringstream read(line) ; read >> word ; ) { 
@@ -137,12 +140,13 @@ public:
                 } 
                 lines->insert(n);
             }
+            */
         }
     }
     QueryResult query(const std::string &) const;
 private:
-    StrVec sv;
-    std::shared_ptr<std::vector<std::string>> svec;
+    //StrVec sv;
+    std::shared_ptr<StrVec> svec;
     std::map<std::string, std::shared_ptr<std::set<std::vector<std::string>::size_type>>> smap;
 };
 
@@ -150,12 +154,12 @@ class QueryResult {
     friend std::ostream &print(std::ostream &, const QueryResult &);
 public:
     QueryResult(const std::string &w,
-                std::shared_ptr<std::vector<std::string>> v,
+                std::shared_ptr<StrVec> v,
                 std::shared_ptr<std::set<std::vector<std::string>::size_type>> s) :
                 word(w), qrvec(v), qrs(s) { };
 private:
     std::string word;
-    std::shared_ptr<std::vector<std::string>> qrvec;
+    std::shared_ptr<StrVec> qrvec;
     std::shared_ptr<std::set<std::vector<std::string>::size_type>> qrs;
 };
 
@@ -199,7 +203,8 @@ void runQueries(std::ifstream &inFile) {
 
 int main()
 {
-
+    std::ifstream open("test.txt");;
+    runQueries(open);
 
 
     return 0;
